b1d93b28aa650fe625368baed525eadba08f2dd5
github username <github email address>
2017-09-06 07:43:28 -0400

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,34 @@
 class BankAccount
 
+  attr_accessor :bank_account, :balance, :status
+  attr_reader :name
+
+  def initialize(name)
+    @bank_account = bank_account
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(money)
+    @balance += money
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    puts "this is super great"
+    if @balance > 0 && @status == "open"
+      true
+    else
+      false
+   end
+  end
+
+  def close_account
+    @status = "closed"
+  end
+
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,51 @@
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :amount
+  attr_reader :status
+
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+    @transaction_record = []
+  end
+
+  def valid?
+    @sender.valid? && @receiver.valid?
+  end
+
+  def execute_transaction
+    if @amount > @sender.balance
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    elsif @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @receiver.balance
+      @status = "complete"
+      @transaction_record << @amount
+    else
+      @status == "rejected"
+    end
+  end
+
+  def reverse_transfer
+    if !@transaction_record.empty?
+      returned_amount = @transaction_record.last
+      @sender.balance += returned_amount
+      @receiver.balance -= returned_amount
+      @status = "reversed"
+      @transaction_record.pop
+    end
+  end
+
+
 end
+
+
+
+# Transfers start out in a "pending" status. They can be executed and go to a "complete" state.
+# They can also go to a "rejected" status. A completed transfer can also be reversed and go into a
+# "reversed" status.
